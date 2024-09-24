8be60cea697fae6739502e7ce8efa5af47b24d39
Dario Contreras <thedaroo81@gmail.com>
2024-09-24 09:22:07 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.enableCors({
+    origin: true,
+    methods: 'GET,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
