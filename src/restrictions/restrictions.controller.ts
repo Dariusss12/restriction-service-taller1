4d36d32985d2a8dd16ed09badbe6ad041025d6cb
Dario Contreras <thedaroo81@gmail.com>
2024-09-24 00:14:49 -0300

    fix: endpoints

--- src/restrictions/restrictions.controller.ts
+++ src/restrictions/restrictions.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from
 import { RestrictionsService } from './restrictions.service';
 import { CreateRestrictionDto } from './dto/create-restriction.dto';
 
-@Controller('restrictions-service/students')
+@Controller('restrictions-service')
 export class RestrictionsController {
   constructor(private readonly restrictionsService: RestrictionsService) {}
 
-  @Post(':studentId/restrictions/add')
+  @Post('students/:studentId/restrictions/add')
   create(
     @Param('studentId', ParseUUIDPipe) studentId: string,
     @Body() createRestrictionDto: CreateRestrictionDto
@@ -14,17 +14,17 @@ export class RestrictionsController {
     return this.restrictionsService.addRestriction(createRestrictionDto, studentId);
   }
 
-  @Get(':studentId/restrictions')
+  @Get('students/:studentId/restrictions')
   findAll(@Param('studentId', ParseUUIDPipe) studentId: string) {
     return this.restrictionsService.findAllRestrictions(studentId);
   }
 
-  @Get(':studentId/check-restrictions')
+  @Get('students/:studentId/check-restrictions')
   check(@Param('studentId') studentId: string) {
     return this.restrictionsService.checkRestrictions(studentId);
   }
 
-  @Delete(':studentId/restrictions/:id')
+  @Delete('students/:studentId/restrictions/:id')
   remove(
     @Param('id', ParseUUIDPipe) id: string,
     @Param('studentId', ParseUUIDPipe) studentId: string
