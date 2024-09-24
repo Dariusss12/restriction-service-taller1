56ce16052147b47f179562ba53536a5f32b85740
Dario Contreras <thedaroo81@gmail.com>
2024-09-24 09:41:20 -0300

    refactor: remove cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: true,
-    methods: 'GET,POST,DELETE',
-    credentials: true,
-  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
